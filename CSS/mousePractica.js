76a7df3f2ded51e942029b968622e307e4eaf6d1
Negocio <albocamposa@gmail.com>
2022-02-10 19:24:53 -0300

    Finalizado medio choto con mouse and keyboard

--- CSS/mousePractica.js
+++ CSS/mousePractica.js
@@ -1,10 +1,77 @@
 xx = addEventListener("mousemove", dibujarMouse)
+var cuadrito = document.getElementById("dibujito");
+var papel = cuadrito.getContext("2d");
+yy = addEventListener("mousemove", dibujarMouse)
+document.addEventListener("keydown", dibujarTeclado);
+let x,y;
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+  };
 
-function dibujarMouse(event)
-{   
-    if(event.buttons == 1)
+function dibujarMouse(evento)
+{
+    
+    if(evento.buttons == 1)
     {
-        console.log(event)
+        dibujarLinea("green",x,y,evento.layerX,evento.layerY, papel )
+        x = evento.layerX;
+        y = evento.layerY;
+    }
+    else
+    {
+        x = evento.layerX;
+        y = evento.layerY;
     }
 }
-    
+
+
+function bordes()
+{
+    dibujarLinea("black", 1,1,300,1,papel)
+    dibujarLinea("black", 300,1,300,300,papel)
+    dibujarLinea("black", 1,300,300,300,papel)
+    dibujarLinea("black", 1,1,1,300,papel)
+}
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+bordes()
+dibujarLinea("red", 149, 149, 151, 151, papel)
+
+
+function dibujarTeclado(evento)
+{
+  var colorcito = "#FAA";
+  var movimiento = 5;
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x = x + movimiento;
+    break;
+  }
+}
