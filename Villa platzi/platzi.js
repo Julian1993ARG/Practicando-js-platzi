d94c50ec04a7dac129f7585c2c0d91809b93ceeb
Negocio <albocamposa@gmail.com>
2022-02-16 11:58:59 -0300

    villa platzi V2

--- Villa platzi/platzi.js
+++ Villa platzi/platzi.js
@@ -1,15 +1,50 @@
 let vp = document.getElementById("villaplatzi");
-let papel = vp.getContext("2d")
-let mapa = "imagenes/tile.png";
+let papel = vp.getContext("2d");
 
-let fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener("load", dibujar);
 
-function dibujar (event ) 
+
+
+let fondo =
 {
-  papel.drawImage(fondo, 0 , 0);
-  
+  url: "imagenes/tile.png",
+  cargaOk: false
+}
+let vaca =
+{
+  url: "imagenes/vaca.png",
+  cargaOk: false
+}
+let cerdo =
+{
+  url: "imagenes/cerdo.png",
+  cargaOk: false
+}
+let pollo =
+{
+  url: "imagenes/pollo.png",
+  cargaOk: false
+}
+let perro =
+{
+  url: "imagenes/perro.png",
+  cargaOk: false
+}
+fondo.imagen = new Image();
+fondo.imagen = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo)
+
+function cargarFondo()
+{
+  fondo.cargaOk = true;
+  dibujar()
+}
+
+
+
+function dibujar () 
+{
+  if(fondo.cargaOk)
+    papel.drawImage(fondo.url, 0 , 0);
 }
 
 function aleatorio (min, max)
@@ -18,3 +53,6 @@ function aleatorio (min, max)
   resultado = Math.floor(Math.random() * (max - min +1))+ min;
   return resultado;
 }
+
+
+console.log(fondo)
